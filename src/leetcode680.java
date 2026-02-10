e35d8f4f7574db259e58f81423d1681c4ee34ba4
anshulrawat7 <anshulrawat87088@gmail.com>
2026-02-10 23:07:57 +0530

    init commit

---
+++ src/leetcode680.java
@@ -0,0 +1,37 @@
+public class leetcode680 {
+    public static void main(String[] args) {
+
+    }
+
+
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                // Try skipping either left or right character
+                return isPalindrome(s, left + 1, right) ||
+                        isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+}
+
+
+
+
