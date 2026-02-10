4e020f79d63fa2c65da2bbcd0b4b61ce32026504
anshulrawat7 <anshulrawat87088@gmail.com>
2026-02-10 22:48:31 +0530

    init commit

---
+++ src/leetcode709.java
@@ -0,0 +1,11 @@
+public class leetcode709 {
+    public static void main(String[] args) {
+        String str="AbCSD";
+        System.out.println(tolower(str));
+
+    }
+    static String tolower(String s){
+        String str=s.toLowerCase();
+        return str;
+    }
+}
