a283695ceb2ac0b440695569b57a8c91b782eebf
Anshul Rawat <anshulrawat87088@gmail.com>
2026-08-24 12:31:45 +0530

    Time: 27 ms (49.59%) | Memory: 74.8 MB (95.97%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,5 +1,4 @@
-
-        class Solution {
+class Solution {
     public int[] sortArray(int[] nums) {
         if(nums.length==1){
             return nums;
@@ -7,40 +6,39 @@
         int mid=nums.length/2;
         
         int[] left=sortArray(Arrays.copyOfRange(nums,0,mid));
-        int[] right=sortArray(Arrays.copyOfRange(nums,mid, nums.length));
+        int[] right=sortArray(Arrays.copyOfRange(nums,mid,nums.length));
 
         return merge(left, right);
-    }
-    private static int[] merge(int[] first, int[] second){
-        int[] mix=new int[first.length+second.length];
+        }
+        private static int[] merge(int[] first, int[] second){
+            int[] mix=new int[first.length+second.length];
 
-        int i=0;
-        int j=0;
-        int k=0;
+            int i=0;
+            int j=0;
+            int k=0;
 
-        while(i<first.length && j<second.length){
-            if(first[i]<second[j]){
+            while(i<first.length && j<second.length){
+                if(first[i]<second[j]){
+                    mix[k]=first[i];
+                    i++;
+                }else{
+                    mix[k]=second[j];
+                    j++;
+                }
+                k++;
+            }
+
+            //for the elements that are left in the array 
+            while(i<first.length){
                 mix[k]=first[i];
                 i++;
-            }else{
+                k++;
+            }
+            while(j<second.length){
                 mix[k]=second[j];
                 j++;
+                k++;
             }
-            k++;
+            return mix;
         }
-        //if one of the array is not complete and elements are left behind
-        //add every left element at the back of array
-        while(i<first.length){
-            mix[k]=first[i];
-            i++;
-            k++;
-        }
-        while(j<second.length){
-            mix[k]=second[j];
-            j++;
-            k++;
-        }
-        return mix;
     }
-}
-  
